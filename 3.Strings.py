c033a92ce6e6c8137de7a840faa98af747a9564b
sanjay kumar k p <49227865+sanjay-3129@users.noreply.github.com>
2019-12-29 09:52:01 +0530

    Add files via upload

---
+++ 3.Strings.py
@@ -0,0 +1,37 @@
+#strings creations
+print('python is fan!')
+
+print('He\'s a coder')
+
+print("He's a coder")
+
+print('''He's a coder''')
+
+''' haiiii'''
+
+print('I said "ketchup"!')
+
+print('''I said "ketchup"!''')
+
+print("I said 'ketchup'")
+
+print(1+1)
+
+print("1+1")
+
+print("bommi","chellam","love u", sep='+', end='!')
+
+print("san","chellam","love u")
+
+print("""
+   Ha-ah!
+   This 
+     ...is Hello
+   World! 
+   1
+     2
+       3
+         4
+""")
+
+#...and it would display just as you've typed it, preserving the spaces and newlines.
