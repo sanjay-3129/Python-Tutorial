b2d1127642465a483aada87ef9d3aa629831aa31
sanjay kumar k p <49227865+sanjay-3129@users.noreply.github.com>
2019-12-29 09:52:20 +0530

    Add files via upload

---
+++ 4.Input_Output.py
@@ -0,0 +1,28 @@
+#input and output
+
+#input
+a = input("enter no: ")
+
+#output
+print(a)
+
+#example-0
+name = 'sanjay'
+age = 21
+print('%s is my name and %d is my age.' %(name, age))
+
+#example-1
+a1 = input()
+b1 = input()
+print(int(a1)+ int(b1) + 10)
+
+#example-2
+a2 = int(input())
+b2 = int(input())
+print(a2+b2)
+
+#example-3
+a3 = chr(int(input())) 
+b3 = chr(int(input()))
+print(a3+b3)
+print(ord(a3)+ord(b3))
