1e90feadd64ad484947e591c9eae08218be68d9a
sanjay kumar k p <49227865+sanjay-3129@users.noreply.github.com>
2019-12-29 09:52:32 +0530

    Add files via upload

---
+++ 5.String_Operations.py
@@ -0,0 +1,31 @@
+#string operations
+
+# 1. Concatenation
+
+print("spam"+"eggs")
+print("spam","eggs")
+print("spam"+" "+'eggs')
+print("spam"+", "+"eggs") 
+print("spam"+"," , "eggs")
+
+print(2+3)
+print('2'+'3')
+
+print(1 + int('2') + 3 + int('4'))
+print(str(1) + '2' + str(3) + '4')
+
+#String multiplied by integers to produce their copies
+print("sanjay "*3)
+print(4 * '2')
+print("hi"*0)
+a = "hi"
+print(a*0)
+
+#python strings
+string = "sanjay"
+print (string)
+print (string[0])
+print (string[2:5]) #slice operator
+print (string[2:])
+print (string * 2) #repetiton operator
+print (string +' '+ "handsome") #concatenation operator
