151f9f89779d9dcddd8f488829787bd35e7a6a11
sanjay kumar k p <49227865+sanjay-3129@users.noreply.github.com>
2019-12-29 09:51:35 +0530

    Add files via upload

---
+++ 2.Simple_Operations.py
@@ -0,0 +1,31 @@
+#simple operations
+print(2+2)
+print(2 + 5 * 10)
+print(5 * 10 + 2)
+
+print(2*(3+4))
+print(10/2)
+print(10//2)
+
+print(-7)
+print(-5+5)
+print(-7-7)
+print((-7+2)*(-4))
+
+# print(11/0) -> it produces ZeroByDivisionError
+
+print(6*7.0)
+
+#exponention
+
+print(2**5)
+print(2**1/2)
+print(3**(3**3))
+print(3**3**3)
+print(8**(1/3))
+print(9**0.5)
+print(9**(1/2))
+
+#quotient & remainder
+print(20//6)
+print(1.25 % 0.5)
