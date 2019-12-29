4a84633f53456b8f303f33e308124756a90a052f
sanjay kumar k p <49227865+sanjay-3129@users.noreply.github.com>
2019-12-29 09:54:01 +0530

    Add files via upload

---
+++ 9.Booleans.py
@@ -0,0 +1,33 @@
+#Booleans
+#1.equal operator(==)
+print("a"=="a")
+print('a'=="a")
+print(2==4)
+if((2==4)):
+	print("true")
+else:
+	print("false")
+print(9.0000000000000001 == 9.0)
+
+print()
+
+#2.not equal operator(!=)
+print(2!=3)
+print(5!=5)
+print("seven"!="Seven")
+
+print()
+
+#3.Comparison (< or >)
+print(5<10)
+print('a'>"b")
+print(6<6.00000000000001) #more than 15digits it is showing as false
+
+print()
+
+
+#4.Comparison (<= or >=)
+print(5<=5)
+print(10<=5)
+print(2.0>=2)
+print("abd"<="abc")
