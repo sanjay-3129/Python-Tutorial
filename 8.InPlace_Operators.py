767592ee698ad9f832d9ef86ee12a25a735b8d89
sanjay kumar k p <49227865+sanjay-3129@users.noreply.github.com>
2019-12-29 09:53:33 +0530

    Add files via upload

---
+++ 8.InPlace_Operators.py
@@ -0,0 +1,17 @@
+#in-place operators
+x = 2
+print(x)
+x = x+2
+print(x)
+
+for num in range(0,10):
+	num += 1
+	print(num)
+
+#in strings also we can use
+name = "sanjay"
+print(name)
+name+="kumar"  #name = name + "kumar"
+print(name)
+
+# x++ - python does not have increment operator only in-place operator like (x+=1 or x = x+1)
